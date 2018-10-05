88cd2d6e8c8a57f247c0056d603f79cf5d5d6413
1702025 <msalman98@yahoo.com>
2018-10-05 18:01:40 +0500

    complete task1

--- task1/task1.cpp
+++ task1/task1.cpp
@@ -3,17 +3,31 @@ using namespace std;
  class find_area
  {
    public:
-     find_area()
+     find_area(int len, int wid)
      {
-
+       length=len;
+       wide=wid;
+     }
+     double setarea()
+     {
+       area=length*wide;
+     }
+     double getarea()
+     {
+       return area;
      }
 
 
  private:
+   double area;
    double length;
    double wide;
  };
- int int main(int argc, char const *argv[]) {
-   find_area 
+int main(int argc, char const *argv[])
+ {
+   find_area acct(30,20);
+   acct.setarea();
+   std::cout<<"area ofthe rectangle = "<<acct.getarea();
+
    return 0;
  }
