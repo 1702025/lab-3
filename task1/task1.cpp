0e6f2ffa5e5de72a7d4d30d75b9fe2819bba4211
1702025 <msalman98@yahoo.com>
2018-10-05 17:45:16 +0500

    first time

---
+++ task1/task1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+ class find_area
+ {
+   public:
+     find_area()
+     {
+
+     }
+
+
+ private:
+   double length;
+   double wide;
+ };
+ int int main(int argc, char const *argv[]) {
+   find_area 
+   return 0;
+ }
